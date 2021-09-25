a3ed0f75a0e3b9cd9af7123adda32bfde9070b11
Nandinimatthew <89566551+Nandinimatthew@users.noreply.github.com>
2021-09-25 00:35:43 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <?php
 session_start();
+error_reporting(0);
 include("include/config.php");
 if(isset($_POST['submit']))
 {
-$ret=mysql_query("SELECT * FROM doctors WHERE docEmail='".$_POST['username']."' and password='".md5($_POST['password'])."'");
+$ret=mysql_query("SELECT * FROM admin WHERE username='".$_POST['username']."' and password='".$_POST['password']."'");
 $num=mysql_fetch_array($ret);
 if($num>0)
 {
-$extra="dashboard.php";
-$_SESSION['dlogin']=$_POST['username'];
+$extra="dashboard.php";//
+$_SESSION['login']=$_POST['username'];
 $_SESSION['id']=$num['id'];
-$uip=$_SERVER['REMOTE_ADDR'];
-$status=1;
-$log=mysql_query("insert into doctorslog(uid,username,userip,status) values('".$_SESSION['id']."','".$_SESSION['dlogin']."','$uip','$status')");
 $host=$_SERVER['HTTP_HOST'];
 $uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
 header("location:http://$host$uri/$extra");
@@ -20,13 +18,9 @@ exit();
 }
 else
 {
-$host  = $_SERVER['HTTP_HOST'];
-$_SESSION['dlogin']=$_POST['username'];
-$uip=$_SERVER['REMOTE_ADDR'];
-$status=0;
-mysql_query("insert into doctorslog(username,userip,status) values('".$_SESSION['dlogin']."','$uip','$status')");
 $_SESSION['errmsg']="Invalid username or password";
 $extra="index.php";
+$host  = $_SERVER['HTTP_HOST'];
 $uri  = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
 header("location:http://$host$uri/$extra");
 exit();
@@ -38,7 +32,7 @@ exit();
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title>Doctor Login</title>
+		<title>Admin-Login</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
 		<meta name="apple-mobile-web-app-capable" content="yes">
@@ -60,7 +54,7 @@ exit();
 		<div class="row">
 			<div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 				<div class="logo margin-top-30">
-					<h2> HMS | Doctor Login</h2>
+				<h2>Admin Login</h2>
 				</div>
 
 				<div class="box-login">
@@ -71,7 +65,7 @@ exit();
 							</legend>
 							<p>
 								Please enter your name and password to log in.<br />
-								<span style="color:red;"><?php echo $_SESSION['errmsg']; ?><?php echo $_SESSION['errmsg']="";?></span>
+								<span style="color:red;"><?php echo htmlentities($_SESSION['errmsg']); ?><?php echo htmlentities($_SESSION['errmsg']="");?></span>
 							</p>
 							<div class="form-group">
 								<span class="input-icon">
@@ -80,8 +74,7 @@ exit();
 							</div>
 							<div class="form-group form-actions">
 								<span class="input-icon">
-									<input type="password" class="form-control password" name="password" placeholder="Password">
-									<i class="fa fa-lock"></i>
+									<input type="password" class="form-control password" name="password" placeholder="Password"><i class="fa fa-lock"></i>
 									 </span>
 							</div>
 							<div class="form-actions">
@@ -91,7 +84,6 @@ exit();
 								</button>
 							</div>
 							
-						
 						</fieldset>
 					</form>
 
